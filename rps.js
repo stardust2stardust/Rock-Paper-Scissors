65c59192eac0f1330c860a34a159ae5bc607b599
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-11 12:35:14 -0400

    added game function

--- rps.js
+++ rps.js
@@ -5,33 +5,59 @@ function computerPlay() {
 }
 
 function playOneRound(playerSelection, computerSelection) {
+
     if (computerSelection === playerSelection) {
         return "It is a tie.";
     } else if (computerSelection === "rock") {
         if (playerSelection === "paper") {
-            return `You win! ${playerSelection} beats ${computerSelection}!`;
+            return "player";
         } else if (playerSelection === "scissors") {
-            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+            return "computer";
         }
     } else if (computerSelection === "paper") {
         if (playerSelection === "scissors") {
-            return `You win! ${playerSelection} beats ${computerSelection}!`;
+            return "player";
         } else if (playerSelection === "rock") {
-            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+            return "computer";
         }
     } else if (computerSelection === "scissors") {
         if (playerSelection === "rock") {
-            return `You win! ${playerSelection} beats ${computerSelection}!`;
+            return "player"
         } else if (playerSelection === "paper") {
-            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+            return "computer";
         }
     } 
     }
 
+function game() {
 
+    let playerScore = 0;
+    let compScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Enter one of the following: Rock, Paper, Scissors.').toLowerCase();
+        console.log(`You threw ${playerSelection}`);
+        const computerSelection = computerPlay();
+        console.log(`The computer threw ${computerSelection}`)
+        let winner = playOneRound(playerSelection, computerSelection);
+        if (winner === "player") {
+            playerScore++;
+        } else if (winner === "computer") {
+            compScore++;
+        }
+        console.log(`Player: ${playerScore}    Computer: ${compScore}`)
+    }
+    if (playerScore > compScore) {
+        console.log("You win!");
+    } else if (playerScore < compScore) {
+        console.log("Computer wins!");
+    } else {
+        console.log("It's a tie.")
+    }
+}
 
-const playerSelection = prompt('Enter one of the following: Rock, Paper, Scissors.').toLowerCase()
-console.log(playerSelection)
-const computerSelection = computerPlay()
-console.log(computerSelection)
-console.log(playOneRound(playerSelection, computerSelection))
+//const playerSelection = prompt('Enter one of the following: Rock, Paper, Scissors.').toLowerCase()
+//console.log(playerSelection)
+//const computerSelection = computerPlay()
+//console.log(computerSelection)
+//console.log(playOneRound(playerSelection, computerSelection))
+game()
