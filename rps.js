a574cd20c00f0990818cbe99c59708e9b7ed72f4
Jeni Hoyt <ziahrahl@gmail.com>
2022-04-19 16:42:55 -0400

    add UI components and modified js

--- rps.js
+++ rps.js
@@ -1,10 +1,19 @@
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
+const rounds = document.querySelector('#rounds');
+const final = document.querySelector('#finalWinner');
+const playerCount = document.querySelector('#playerScore');
+const compCount = document.querySelector('#compScore');
+let playerScore = 0;
+let compScore = 0;
+let playerSelection = '';
+let isGameOver = false;
+roundCount = 0;
 
 function computerPlay() {
     const tools = ['rock', 'paper', 'scissors'];
-    let choice = Math.floor(Math.random()*3);
+    let choice = Math.floor(Math.random() * 3);
     return tools[choice];
 }
 
@@ -30,50 +39,8 @@ function playOneRound(playerSelection, computerSelection) {
             return "computer";
         }
     }
-} 
+}
 
-let playerScore = 0;
-let compScore = 0;
-let playerSelection = ''
-
-let click_rock = rock.addEventListener('click', () => {
-    click_rock = rock.outerText.toLowerCase();
-    playerSelection = click_rock
-    computerSelection = computerPlay();
-    let winner = playOneRound(playerSelection, computerSelection);
-    if (winner === "player") {
-        playerScore++;
-    } else if (winner === "computer") {
-        compScore++;
-    }
-    console.log(`Player: ${playerScore}    Computer: ${compScore}`)
-});
-
-let click_paper = paper.addEventListener('click', () => {
-    click_paper = paper.outerText.toLowerCase();
-    playerSelection = click_paper
-    computerSelection = computerPlay();
-    let winner = playOneRound(playerSelection, computerSelection);
-    if (winner === "player") {
-        playerScore++;
-    } else if (winner === "computer") {
-        compScore++;
-    }
-    console.log(`Player: ${playerScore}    Computer: ${compScore}`)
-});
-
-let click_scissors = scissors.addEventListener('click', () => {
-    click_scissors = scissors.outerText.toLowerCase();
-    playerSelection = click_scissors
-    computerSelection = computerPlay();
-    let winner = playOneRound(playerSelection, computerSelection);
-    if (winner === "player") {
-        playerScore++;
-    } else if (winner === "computer") {
-        compScore++;
-    }
-    console.log(`Player: ${playerScore}    Computer: ${compScore}`)
-});
 function displayWinner(playerScore, compScore) {
     if (playerScore > compScore) {
         console.log("You win!");
@@ -83,4 +50,84 @@ function displayWinner(playerScore, compScore) {
         console.log("It's a tie.");
     }
 }
+
+
+let click_rock = rock.addEventListener('click', () => {
+    roundCount += 1
+    rounds.innerText = roundCount;
+    if (!isGameOver) {
+        click_rock = rock.outerText.toLowerCase();
+        playerSelection = click_rock;
+        computerSelection = computerPlay();
+        let winner = playOneRound(playerSelection, computerSelection);
+        if (winner === 'player') {
+            playerScore++;
+            playerCount.innerText = playerScore;
+        } else {
+            compScore++;
+            compCount.innerText = compScore;
+        }
+
+        if (playerScore === 5 || compScore === 5) {
+            isGameOver = true;
+            if (playerScore > compScore) {
+                final.innerText = "You win!";
+            } else {
+                final.innerText = "You lose!";
+            }
+        }
+    }
+});
+
+let click_paper = paper.addEventListener('click', () => {
+    roundCount += 1
+    rounds.innerText = roundCount;
+    if (!isGameOver) {
+        click_paper = paper.outerText.toLowerCase();
+        playerSelection = click_paper
+        computerSelection = computerPlay();
+        let winner = playOneRound(playerSelection, computerSelection);
+        if (winner === 'player') {
+            playerScore++;
+            playerCount.innerText = playerScore;
+        } else {
+            compScore++;
+            compCount.innerText = compScore;
+        }
+        if (playerScore === 5 || compScore === 5) {
+            isGameOver = true;
+            if (playerScore > compScore) {
+                final.innerText = "You win!";
+            } else {
+                final.innerText = "You lose!";
+            }
+        }
+    }
+});
+
+let click_scissors = scissors.addEventListener('click', () => {
+    roundCount += 1
+    rounds.innerText = roundCount;
+    if (!isGameOver) {
+        click_scissors = scissors.outerText.toLowerCase();
+        playerSelection = click_scissors
+        computerSelection = computerPlay();
+        let winner = playOneRound(playerSelection, computerSelection);
+        if (winner === 'player') {
+            playerScore++;
+            playerCount.innerText = playerScore;
+        } else {
+            compScore++;
+            compCount.innerText = compScore;
+        }
+        if (playerScore === 5 || compScore === 5) {
+            isGameOver = true;
+            if (playerScore > compScore) {
+                final.innerText = "You win!";
+            } else {
+                final.innerText = "You lose!";
+            }
+        }
+    }
+});
 
